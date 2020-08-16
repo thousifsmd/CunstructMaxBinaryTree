609a8131fdf2cf52b46383341e29e075ecd87fe3
thousifsmd <thousifsmd@gmail.com>
2020-08-15 19:54:52 -0500

    Update MaxBinaryTree.java

--- MaxBinaryTree.java
+++ MaxBinaryTree.java
@@ -1,5 +1,6 @@
+// LeetCode #654 - https://leetcode.com/problems/maximum-binary-tree/submissions/
 /**
- * LeetCode - https://leetcode.com/problems/maximum-binary-tree/submissions/
+ * 
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -16,41 +17,36 @@
  */
 class MaxBinaryTree {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-    
-		TreeNode root = binaryTree(nums, 0, nums.length-1);
-		return root;
+	TreeNode root = binaryTree(nums, 0, nums.length-1);
+	return root;
 		
     }
 	
-	
-	public TreeNode binaryTree(int[] nums, int start, int end) {
-	
-		int index = findMax(nums, start, end);
-		
-		if(start > index ) return null;
-		if(index > end ) return null;
-		
-		TreeNode root = new TreeNode(nums[index]);
-		root.left = binaryTree(nums, start, index-1);
-		root.right = binaryTree(nums, index+1, end);
-		
-		return root;
-	}
+    public TreeNode binaryTree(int[] nums, int start, int end) {
+        int index = findMax(nums, start, end);
+
+	if(start > index ) return null;
+	if(index > end ) return null;
+
+	TreeNode root = new TreeNode(nums[index]);
+	root.left = binaryTree(nums, start, index-1);
+	root.right = binaryTree(nums, index+1, end);
+
+	return root;
+    }
 	
 	
-	public int findMax(int[] nums, int start, int end) {
-		int max = -1;
-		int index = 0;
-		
-		for(int i=start; i <= end; i++) {
-			if(nums[i] > max) {
-				max = nums[i];
-				index = i;
-			}
+    public int findMax(int[] nums, int start, int end) {
+	int max = -1;
+	int index = 0;
+
+	for(int i=start; i <= end; i++) {
+		if(nums[i] > max) {
+			max = nums[i];
+			index = i;
 		}
-		
-		return index;
-		
 	}
+	return index;
+    }
 	
 }
